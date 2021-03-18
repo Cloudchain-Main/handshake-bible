9272e2581a314483dc92ea063389354a1f4ea61b
Facinorous <admin@blockshelf.ca>
2021-03-18 14:32:51 -0400

    add quote

--- hnsBibleQuotes.ts
+++ hnsBibleQuotes.ts
@@ -1,13 +1,22 @@
-import { IQuotes } from './typings/Quotes'
+import { IQuotes } from "./typings/Quotes"
 
 const hnsBibleQuotes: IQuotes[] = [
   {
     quote: `HNS includes all blockchain domains, including .eth... they could embrace that reality more fully. To not collaborate in 2021 makes no sense as a community building narrative.`,
-    quotee: 'HandshkeJesus',
-    location: 'Discord',
-    link: 'https://discord.com/channels/664247448469897267/774879082831413259/822114264424513590',
-    date: new Date(2021, 3, 18)
-  }
+    quotee: "HandshkeJesus",
+    location: "Discord",
+    link:
+      "https://discord.com/channels/664247448469897267/774879082831413259/822114264424513590",
+    date: new Date(2021, 3, 18),
+  },
+  {
+    quote: `All roads lead to the root zone.`,
+    quotee: "HandshkeJesus",
+    location: "Discord",
+    link:
+      "https://discord.com/channels/664247448469897267/774879082831413259/822125721076891658",
+    date: new Date(2021, 3, 18),
+  },
 ]
 
 export default hnsBibleQuotes
