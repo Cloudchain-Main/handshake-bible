24359de4e82ba6f6560e9a83ae6d09bb43a86ceb
Facinorous <admin@blockshelf.ca>
2021-03-19 00:09:36 -0400

    fix names

--- hnsBibleQuotes.ts
+++ hnsBibleQuotes.ts
@@ -3,7 +3,7 @@ import { IQuotes } from "./typings/Quotes"
 const hnsBibleQuotes: IQuotes[] = [
   {
     quote: `HNS includes all blockchain domains, including .eth... they could embrace that reality more fully. To not collaborate in 2021 makes no sense as a community building narrative.`,
-    quotee: "HandshkeJesus",
+    quotee: "HandshakeJesus",
     location: "Discord",
     link:
       "https://discord.com/channels/664247448469897267/774879082831413259/822114264424513590",
@@ -11,7 +11,7 @@ const hnsBibleQuotes: IQuotes[] = [
   },
   {
     quote: `All roads lead to the root zone.`,
-    quotee: "HandshkeJesus",
+    quotee: "HandshakeJesus",
     location: "Discord",
     link:
       "https://discord.com/channels/664247448469897267/774879082831413259/822125721076891658",
@@ -19,7 +19,7 @@ const hnsBibleQuotes: IQuotes[] = [
   },
   {
     quote: `Like many, when I saw Bitcoin, I was willing to give my life for its survival. I feel that again.`,
-    quotee: "HandshkeJesus",
+    quotee: "HandshakeJesus",
     location: "Discord",
     link:
       "https://discord.com/channels/664247448469897267/774879082831413259/795327270641467422",
@@ -27,7 +27,7 @@ const hnsBibleQuotes: IQuotes[] = [
   },
   {
     quote: `I believe that something big is happening now, and here.`,
-    quotee: "HandshkeJesus",
+    quotee: "HandshakeJesus",
     location: "Discord",
     link:
       "https://discord.com/channels/664247448469897267/774879082831413259/799027171716628481",
@@ -35,7 +35,7 @@ const hnsBibleQuotes: IQuotes[] = [
   },
   {
     quote: `A visionary leader sees the future and creates the conditions for it to manifest.`,
-    quotee: "HandshkeJesus",
+    quotee: "HandshakeJesus",
     location: "Discord",
     link:
       "https://discord.com/channels/664247448469897267/774879082831413259/799381622582673408",
@@ -43,7 +43,7 @@ const hnsBibleQuotes: IQuotes[] = [
   },
   {
     quote: `The beauty of miscommunications is the opportunity to resolve the miscommunications with more communication.`,
-    quotee: "HandshkeJesus",
+    quotee: "HandshakeJesus",
     location: "Discord",
     link:
       "https://discord.com/channels/664247448469897267/774879082831413259/799358884337680445",
@@ -51,7 +51,7 @@ const hnsBibleQuotes: IQuotes[] = [
   },
   {
     quote: `Even Jesus loves milfs/`,
-    quotee: "HandshkeJesus",
+    quotee: "HandshakeJesus",
     location: "Discord",
     link:
       "https://discord.com/channels/664247448469897267/774879082831413259/799683155313033246",
@@ -59,7 +59,7 @@ const hnsBibleQuotes: IQuotes[] = [
   },
   {
     quote: `I’m not here to make money. I’m here to assist in the evolution (development) of humanity.`,
-    quotee: "HandshkeJesus",
+    quotee: "HandshakeJesus",
     location: "Discord",
     link:
       "https://discord.com/channels/664247448469897267/774879082831413259/799682349213941782",
@@ -67,7 +67,7 @@ const hnsBibleQuotes: IQuotes[] = [
   },
   {
     quote: `We need to educate and develop humanity.`,
-    quotee: "HandshkeJesus",
+    quotee: "HandshakeJesus",
     location: "Discord",
     link:
       "https://discord.com/channels/664247448469897267/774879082831413259/799396121104351303",
@@ -75,7 +75,7 @@ const hnsBibleQuotes: IQuotes[] = [
   },
   {
     quote: `The only thing to fear is fear itself.`,
-    quotee: "HandshkeJesus",
+    quotee: "HandshakeJesus",
     location: "Discord",
     link:
       "https://discord.com/channels/664247448469897267/774879082831413259/799754476567003136",
@@ -91,7 +91,7 @@ const hnsBibleQuotes: IQuotes[] = [
   },
   {
     quote: `Web3 is a network. In which each member can own their discrete identity. This is the crystallization of the ether. Almost literally. Like a manifestation of the matrix. It is destined. We just have to play our role... patiently and meticulously... because we are all in a hurry.`,
-    quotee: "HandshkeJesus",
+    quotee: "HandshakeJesus",
     location: "Discord",
     link:
       "https://discord.com/channels/664247448469897267/774879082831413259/799755301112119326",
@@ -99,7 +99,7 @@ const hnsBibleQuotes: IQuotes[] = [
   },
   {
     quote: `Not your keys, not your TLDS`,
-    quotee: "HandshkeJesus",
+    quotee: "HandshakeJesus",
     location: "Discord",
     link:
       "https://discord.com/channels/664247448469897267/774879082831413259/799789926538674176",
@@ -107,7 +107,7 @@ const hnsBibleQuotes: IQuotes[] = [
   },
   {
     quote: `I hate negotiating with snipers...`,
-    quotee: "HandshkeJesus",
+    quotee: "HandshakeJesus",
     location: "Discord",
     link:
       "https://discord.com/channels/664247448469897267/682156961986641920/800168773771722802",
@@ -115,7 +115,7 @@ const hnsBibleQuotes: IQuotes[] = [
   },
   {
     quote: `We do not yet comprehend how this will change the simple cognitive relationship to “domains”... This is an evolutionary tool that humanity is just learning to use. You think coinbase has a lot of assets and users...!?!? We are colonizing virtual space. It is almost immortality. And almost infinite. For simple reference, add up all the money in the world... And then add up the lifetime value of all the property on the earth... And compare. Then add whatever factor you think being virtual adds to the total “space” available, as it doesn’t have 3D limits. I don’t want to live on Mars. I want my feet on the Earth. And my head in the ether... Once the money is in “space”... the attention follows. Then our minds go there... to expand and evolve. First money. Now... real estate in space.`,
-    quotee: "HandshkeJesus",
+    quotee: "HandshakeJesus",
     location: "Discord",
     link:
       "https://discord.com/channels/664247448469897267/682156961986641920/800532676749688912",
@@ -123,7 +123,7 @@ const hnsBibleQuotes: IQuotes[] = [
   },
   {
     quote: `That is why your voice is so important. I appreciate it. Communication is key. The community should have a voice, and it should be like free market research and respected.`,
-    quotee: "HandshkeJesus",
+    quotee: "HandshakeJesus",
     location: "Discord",
     link:
       "https://discord.com/channels/664247448469897267/774879082831413259/801180635460141136",
@@ -131,7 +131,7 @@ const hnsBibleQuotes: IQuotes[] = [
   },
   {
     quote: `Imagine being a NoNamer... Wait. Don’t even. It’s too terrifying.`,
-    quotee: "HandshkeJesus",
+    quotee: "HandshakeJesus",
     location: "Discord",
     link:
       "https://discord.com/channels/664247448469897267/774879082831413259/801750935952162826",
@@ -139,7 +139,7 @@ const hnsBibleQuotes: IQuotes[] = [
   },
   {
     quote: `Energy is neither created nor destroyed only changes form. Electrons into satoshis into dollarydoos.`,
-    quotee: "HandshkeJesus",
+    quotee: "HandshakeJesus",
     location: "Discord",
     link:
       "https://discord.com/channels/664247448469897267/774879082831413259/806654180986454057",
@@ -147,7 +147,7 @@ const hnsBibleQuotes: IQuotes[] = [
   },
   {
     quote: `Handshake dominance is our destiny. But we still need to play our role in the cosmic drama. The revolution already occurred. This is evolution.`,
-    quotee: "HandshkeJesus",
+    quotee: "HandshakeJesus",
     location: "Discord",
     link:
       "https://discord.com/channels/664247448469897267/774879082831413259/807400086882615336",
@@ -155,7 +155,7 @@ const hnsBibleQuotes: IQuotes[] = [
   },
   {
     quote: `I will say this, what we are doing in Handshake is so powerful...  both the angels and demons are here, now.`,
-    quotee: "HandshkeJesus",
+    quotee: "HandshakeJesus",
     location: "Discord",
     link:
       "https://discord.com/channels/664247448469897267/682156961986641920/812886466135982083",
@@ -163,7 +163,7 @@ const hnsBibleQuotes: IQuotes[] = [
   },
   {
     quote: `I want to promote the narrative... plant, and water, the idea... that the .com bubble is going to burst. Why have a slave name? When you can be free.`,
-    quotee: "HandshkeJesus",
+    quotee: "HandshakeJesus",
     location: "Discord",
     link:
       "https://discord.com/channels/664247448469897267/682156961986641920/813060053288222781",
@@ -171,7 +171,7 @@ const hnsBibleQuotes: IQuotes[] = [
   },
   {
     quote: `No better NFT than a word.`,
-    quotee: "HandshkeJesus",
+    quotee: "HandshakeJesus",
     location: "Discord",
     link:
       "https://discord.com/channels/664247448469897267/682156961986641920/813213142451617802",
@@ -179,7 +179,7 @@ const hnsBibleQuotes: IQuotes[] = [
   },
   {
     quote: `I don’t think we will be living on Mars anytime soon... but we are colonizing virtual space... here and now.`,
-    quotee: "HandshkeJesus",
+    quotee: "HandshakeJesus",
     location: "Discord",
     link:
       "https://discord.com/channels/664247448469897267/774879082831413259/813596065714143242",
@@ -187,7 +187,7 @@ const hnsBibleQuotes: IQuotes[] = [
   },
   {
     quote: `I’m just playing my role. We all have our role. I’m grateful to have learned about Handshake. There is something amazing, revolutionary, evolutionary happening here. I thought decentralizing money was worth dying for... We won that. Decentralizing the Internet is our goal now. We will win that too.`,
-    quotee: "HandshkeJesus",
+    quotee: "HandshakeJesus",
     location: "Discord",
     link:
       "https://discord.com/channels/664247448469897267/682156961986641920/815380569227657226",
@@ -195,7 +195,7 @@ const hnsBibleQuotes: IQuotes[] = [
   },
   {
     quote: `It could literal change your identity, how you brand yourself, in your own mind, and in the minds of others. One letter different, is a different identity. It is teaching us all something...`,
-    quotee: "HandshkeJesus",
+    quotee: "HandshakeJesus",
     location: "Discord",
     link:
       "https://discord.com/channels/664247448469897267/774879082831413259/816090542906540113",
@@ -203,7 +203,7 @@ const hnsBibleQuotes: IQuotes[] = [
   },
   {
     quote: `Handshake makes the internet cool again.`,
-    quotee: "HandshkeJesus",
+    quotee: "HandshakeJesus",
     location: "Discord",
     link:
       "https://discord.com/channels/664247448469897267/774879082831413259/819444449074872321",
@@ -211,7 +211,7 @@ const hnsBibleQuotes: IQuotes[] = [
   },
   {
     quote: `Why pay a ransom for your name? Self Sovereign Identity that you can monetize You have never been this free We have never felt this freedom And empowerment`,
-    quotee: "HandshkeJesus",
+    quotee: "HandshakeJesus",
     location: "Discord",
     link:
       "https://discord.com/channels/664247448469897267/774879082831413259/819434985852370944",
@@ -219,7 +219,7 @@ const hnsBibleQuotes: IQuotes[] = [
   },
   {
     quote: `ENS is .eth plus ICANN`,
-    quotee: "HandshkeJesus",
+    quotee: "HandshakeJesus",
     location: "Discord",
     link:
       "https://discord.com/channels/664247448469897267/774879082831413259/819425885525901312",
